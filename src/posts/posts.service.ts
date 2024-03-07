eba1eea56103a25830b71f6948b41a923b71060c
Jeongwoo Kim <corc411@gmail.com>
2024-03-08 00:17:08 +0900

    Refactor getAllPosts and getPostById methods in PostsService

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -12,33 +12,6 @@ export interface PostModel {
   commentCount: number;
 }
 
-const posts: PostModel[] = [
-  {
-    id: 1,
-    author: 'newjeans_official',
-    title: '뉴진스 민지',
-    content: '메이크업 고치고 있는 민지',
-    likeCount: 1000000,
-    commentCount: 999999,
-  },
-  {
-    id: 2,
-    author: 'newjeans_official',
-    title: '뉴진스 해린',
-    content: '노래 연습하고 있는 해린',
-    likeCount: 1000000,
-    commentCount: 999999,
-  },
-  {
-    id: 3,
-    author: 'blackpink_official',
-    title: '블랙핑크 로제',
-    content: '종합운동장에서 공연 중인 로제',
-    likeCount: 1000000,
-    commentCount: 999999,
-  },
-];
-
 @Injectable()
 export class PostsService {
   constructor(
@@ -47,11 +20,14 @@ export class PostsService {
   ) {}
 
   getAllPosts() {
-    return this.postsRepository.find();
+    return this.postsRepository.find({ relations: ['author'] });
   }
 
   async getPostById(id: number) {
-    const post = await this.postsRepository.findOne({ where: { id } });
+    const post = await this.postsRepository.findOne({
+      where: { id },
+      relations: ['author'],
+    });
 
     if (!post) throw new NotFoundException();
 
