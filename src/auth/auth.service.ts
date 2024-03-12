2b1e18a61cb06e5f4958262e5cb928040c3ddcfc
Jeongwoo Kim <corc411@gmail.com>
2024-03-13 00:52:31 +0900

    feat: implement user authentication and registration
    - Add UsersModule to AuthModule imports for dependency injection
    - Implement authenticateWithEmailAndPassword method in AuthService to verify user credentials
    - Add loginWithEmail method in AuthService to authenticate user and generate access and refresh tokens
    - Add registerWithEmail method in AuthService to create a new user and log them in
    - Export UsersService from UsersModule for use in AuthService
    - Add getUserByEmail method in UsersService to find a user by their email
    - Check for existing nicknames and emails during user registration in UsersService to prevent duplicates
    - Throw appropriate exceptions for unauthorized access and bad requests

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import * as bycrypt from 'bcrypt';
 import { UsersModel } from 'src/users/entities/users.entity';
-import { JWT_SECRET } from './const/auth.const';
+import { UsersService } from 'src/users/users.service';
+import { HASH_ROUNDS, JWT_SECRET } from './const/auth.const';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly usersService: UsersService,
+  ) {}
 
   signToken(user: Pick<UsersModel, 'email' | 'id'>, isRefreshToken: boolean) {
     const payload = {
@@ -25,5 +30,37 @@ export class AuthService {
       accessToken: this.signToken(user, false),
       refreshToken: this.signToken(user, true),
     };
+  }
+
+  async authenticateWithEmailAndPassword(
+    user: Pick<UsersModel, 'email' | 'password'>,
+  ) {
+    const existingUser = await this.usersService.getUserByEmail(user.email);
+    if (!existingUser)
+      throw new UnauthorizedException('존재하지 않는 사용자입니다.');
+
+    const passOk = await bycrypt.compare(user.password, existingUser.password);
+    if (!passOk) throw new UnauthorizedException('비밀번호가 틀렸습니다.');
+
+    return existingUser;
+  }
+
+  async loginWithEmail(user: Pick<UsersModel, 'email' | 'password'>) {
+    const existingUser = await this.authenticateWithEmailAndPassword(user);
+
+    return this.loginUser(existingUser);
+  }
+
+  async registerWithEmail(
+    user: Pick<UsersModel, 'nickname' | 'email' | 'password'>,
+  ) {
+    const hash = await bycrypt.hash(user.password, HASH_ROUNDS);
+
+    const newUser = await this.usersService.createUser({
+      ...user,
+      password: hash,
+    });
+
+    return this.loginUser(newUser);
   }
 }
