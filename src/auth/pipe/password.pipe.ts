1a51a5cf962f597fa04ead2d67d820031eeaa2eb
Jeongwoo Kim <corc411@gmail.com>
2024-03-24 20:28:35 +0900

    Fix password length validation in PasswordPipe

--- src/auth/pipe/password.pipe.ts
+++ src/auth/pipe/password.pipe.ts
@@ -7,6 +7,7 @@ import {
 
 @Injectable()
 export class PasswordPipe implements PipeTransform {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   transform(value: any, metadata: ArgumentMetadata) {
     if (value.toString().lenth > 8) {
       throw new BadRequestException('비밀번호는 8자 이하로 입력해주세요.');
