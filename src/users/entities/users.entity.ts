268cb109882575bf769d12956db37f9ce1d5bb50
Jeongwoo Kim <corc411@gmail.com>
2024-03-25 15:26:29 +0900

    feat: add common module with base entity

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -1,13 +1,11 @@
+import { BaseModel } from 'src/common/entity/base.entity';
 import { PostsModel } from 'src/posts/entities/posts.entity';
 import { PostModel } from 'src/posts/posts.service';
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
 import { RolesEnum } from '../const/roles.const';
 
 @Entity()
-export class UsersModel {
-  @PrimaryGeneratedColumn()
-  id: number;
-
+export class UsersModel extends BaseModel {
   @Column({ length: 20, unique: true })
   nickname: string;
 
