677684681e28411253ce2cf2c9cabc6c33af8572
Jeongwoo Kim <corc411@gmail.com>
2024-03-26 00:20:32 +0900

    feat: Add BasicTokenGuard for email login authentication
    - Implement BasicTokenGuard to authenticate user with email and password
    - Apply BasicTokenGuard to the postLoginEmail route in AuthController
    - Add @Request() decorator to access the request object in postLoginEmail

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,13 @@
-import { Body, Controller, Headers, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Headers,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { BasicTokenGuard } from './guard/basic-token.guard';
 import { MaxLengthPipe, MinLengthPipe } from './pipe/password.pipe';
 
 @Controller('auth')
@@ -25,7 +33,8 @@ export class AuthController {
   }
 
   @Post('login/email')
-  postLoginEmail(@Headers('authorization') rawToken: string) {
+  @UseGuards(BasicTokenGuard)
+  postLoginEmail(@Headers('authorization') rawToken: string, @Request() req) {
     const token = this.authService.extractTokenFromHeader(rawToken, false);
 
     const credentials = this.authService.decodeBasicToken(token);
