5935b95501e56b936d61398e3196f0a419fd0e85
Jeongwoo Kim <corc411@gmail.com>
2024-04-14 16:34:16 +0900

    Introduce environmental constants and enhance pagination in PostsService
    This commit introduces two main changes:
    
    1. **Environmental Constants**: A new file `env.const.ts` has been created under `src/common/const` to define environmental constants such as `PROTOCOL` and `HOST`. This approach centralizes the management of such values, making the application easier to configure and maintain.
    
    2. **Enhanced Pagination Logic in PostsService**:
       - The `paginatePosts` method in `PostsService` has been updated to generate a `next` URL for pagination. This URL construction utilizes the newly introduced environmental constants to dynamically build the endpoint URL.
       - The pagination response now includes a `cursor` that indicates the `id` of the last item in the current payload, a `count` of the items returned, and a `next` URL for fetching subsequent posts.
       - This enhancement improves API usability by providing clients with ready-to-use pagination links and clear indicators of the dataset's extent.
    
    These changes are aimed at improving the scalability of the application's configuration and enhancing the user experience by simplifying client-side pagination logic.

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { HOST, PROTOCOL } from 'src/common/const/env.const';
 import { MoreThan, Repository } from 'typeorm';
 import { CreatePostDto } from './dto/create-post.dto';
 import { paginatePostDto } from './dto/paginate-post.dto';
@@ -43,7 +44,30 @@ export class PostsService {
       take: dto.take,
     });
 
-    return { data: posts };
+    const lastItem = posts.length > 0 ? posts[posts.length - 1] : null;
+
+    const nextUrl = lastItem && new URL(`${PROTOCOL}://${HOST}/posts`);
+
+    if (nextUrl) {
+      for (const key of Object.keys(dto)) {
+        if (dto[key]) {
+          if (key !== 'where__id_more_than') {
+            nextUrl.searchParams.append(key, dto[key]);
+          }
+        }
+      }
+      nextUrl.searchParams.append(
+        'where__id_more_than',
+        lastItem.id.toString(),
+      );
+    }
+
+    return {
+      data: posts,
+      cursor: { after: lastItem?.id },
+      count: posts.length,
+      next: nextUrl?.toString(),
+    };
   }
 
   async getPostById(id: number) {
