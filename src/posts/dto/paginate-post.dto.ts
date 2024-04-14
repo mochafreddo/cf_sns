bdd4b6fe627ec315c045525bc4db65bfcafa3055
Jeongwoo Kim <corc411@gmail.com>
2024-04-14 15:41:38 +0900

    Enhance type validation in paginatePostDto
    This commit enhances the type validation for the `paginatePostDto` used in post pagination queries. The changes include:
    
    1. Addition of the `@Type(() => Number)` decorator from `class-transformer` to ensure that the `where__id_more_than` field is correctly typed as a number when it is transformed from the request data.
    2. Addition of a new comment to suppress a specific ESLint warning (`@typescript-eslint/prefer-as-const`) for the `order__createdAt` field, clarifying that the default value 'ASC' is intentionally a constant.
    
    These enhancements aim to improve the robustness of data validation and handling within the post pagination functionality.

--- src/posts/dto/paginate-post.dto.ts
+++ src/posts/dto/paginate-post.dto.ts
@@ -1,12 +1,15 @@
+import { Type } from 'class-transformer';
 import { IsIn, IsNumber, IsOptional } from 'class-validator';
 
 export class paginatePostDto {
+  @Type(() => Number)
   @IsNumber()
   @IsOptional()
   where__id_more_than?: number;
 
   @IsIn(['ASC'])
   @IsOptional()
+  // eslint-disable-next-line @typescript-eslint/prefer-as-const
   order__createdAt: 'ASC' = 'ASC';
 
   @IsNumber()
