6f79e15ab34a99bc6a06332b12ae4dd715a72d12
Jeongwoo Kim <corc411@gmail.com>
2024-04-14 16:49:35 +0900

    Enhance pagination with support for bidirectional cursors
    This commit introduces several enhancements to the pagination capabilities of the PostsService, particularly by supporting both ascending and descending orders of pagination:
    
    1. **DTO Update**: The `paginatePostDto` now includes a new optional field `where__id_less_than` to facilitate pagination in the descending order. This complements the existing `where__id_more_than` for ascending order pagination.
    
    2. **Service Logic Update**: Adjustments in the `paginatePosts` method of the `PostsService`:
       - The pagination URL generation logic now conditionally appends either `where__id_more_than` or `where__id_less_than` based on the `order__createdAt` direction specified in the DTO. This ensures that the correct cursor is used depending on whether the posts are being paginated in ascending or descending order.
       - The `nextUrl` search parameters now exclude appending the cursor ID keys (`where__id_more_than` and `where__id_less_than`) directly, but instead determine the appropriate cursor based on the sorting order (`ASC` or `DESC`).
    
    These changes make the API's pagination more versatile and user-friendly, accommodating different sorting preferences and improving the overall usability of pagination in data retrieval scenarios.

--- src/posts/dto/paginate-post.dto.ts
+++ src/posts/dto/paginate-post.dto.ts
@@ -3,6 +3,10 @@ import { IsIn, IsNumber, IsOptional } from 'class-validator';
 export class paginatePostDto {
   @IsNumber()
   @IsOptional()
+  where__id_less_than?: number;
+
+  @IsNumber()
+  @IsOptional()
   where__id_more_than?: number;
 
   @IsIn(['ASC', 'DESC'] as const)
