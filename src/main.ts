f959a4c222fb61e61b645f674ec8c69e5c8f16ae
Jeongwoo Kim <corc411@gmail.com>
2024-04-14 15:45:40 +0900

    Improve validation and conversion handling in the application
    This commit makes two major changes aimed at enhancing the data validation and type conversion across the application:
    
    1. The global validation pipe configuration in `main.ts` has been updated to include `transformOptions` with `enableImplicitConversion: true`. This change allows for implicit conversion of primitive types, simplifying DTOs and improving robustness in handling user input.
    
    2. The `@Type(() => Number)` decorator from `class-transformer` has been removed from the `paginatePostDto` in `src/posts/dto/paginate-post.dto.ts`. This removal is due to the redundancy after enabling implicit type conversion at the global level, which ensures that numeric types are automatically handled without explicit decorators.
    
    These updates streamline the codebase by reducing redundancy and enhancing the application's ability to correctly and efficiently process input data.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
 
   await app.listen(3000);
 }
--- src/posts/dto/paginate-post.dto.ts
+++ src/posts/dto/paginate-post.dto.ts
@@ -1,8 +1,6 @@
-import { Type } from 'class-transformer';
 import { IsIn, IsNumber, IsOptional } from 'class-validator';
 
 export class paginatePostDto {
-  @Type(() => Number)
   @IsNumber()
   @IsOptional()
   where__id_more_than?: number;
