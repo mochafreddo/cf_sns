92dd10bb7814294de02223c54ede512049ea7f16
Jeongwoo Kim <corc411@gmail.com>
2024-04-15 15:06:11 +0900

    Enhance pagination and refactor field names
    - Introduce a generic pagination method in the CommonService to handle base pagination logic.
    - Adjust the BasePaginationDto to use consistent field naming with double underscores (__) for the 'less than' and 'more than' filters.
    - Refactor PostsService to align with the new DTO field naming conventions and update URL generation logic to support pagination.
    - Minor reordering of imports in PostsController for better code organization.

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -12,12 +12,12 @@ import {
 } from '@nestjs/common';
 import { AccessTokenGuard } from 'src/auth/guard/bearer-token.guard';
 import { User } from 'src/users/decorator/user.decorator';
+import { UsersModel } from 'src/users/entities/users.entity';
 import { CreatePostDto } from './dto/create-post.dto';
 import { paginatePostDto } from './dto/paginate-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { PostsModel } from './entities/posts.entity';
 import { PostsService } from './posts.service';
-import { UsersModel } from 'src/users/entities/users.entity';
 
 @Controller('posts')
 export class PostsController {
