4137105130cb9e59e255f3569cbc6bb6ed89a9c6
Jeongwoo Kim <corc411@gmail.com>
2024-04-15 14:53:46 +0900

    Refactor pagination DTOs to extend BasePaginationDto
    Introduced a new BasePaginationDto in the common/dto directory to centralize the pagination logic. This base DTO includes common pagination properties such as page, where__id_less_than, where__id_more_than, order__createdAt, and take, along with their respective decorators from the class-validator library. Updated paginatePostDto in the posts/dto directory to extend this new BasePaginationDto, removing redundant declarations and imports, to streamline the code and enhance maintainability.

--- src/posts/dto/paginate-post.dto.ts
+++ src/posts/dto/paginate-post.dto.ts
@@ -1,23 +1,3 @@
-import { IsIn, IsNumber, IsOptional } from 'class-validator';
+import { BasePaginationDto } from 'src/common/dto/base-pagination.dto';
 
-export class paginatePostDto {
-  @IsNumber()
-  @IsOptional()
-  page?: number;
-
-  @IsNumber()
-  @IsOptional()
-  where__id_less_than?: number;
-
-  @IsNumber()
-  @IsOptional()
-  where__id_more_than?: number;
-
-  @IsIn(['ASC', 'DESC'] as const)
-  @IsOptional()
-  order__createdAt: 'ASC' | 'DESC' = 'ASC';
-
-  @IsNumber()
-  @IsOptional()
-  take: number = 20;
-}
+export class paginatePostDto extends BasePaginationDto {}
