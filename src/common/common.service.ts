014c242ae8cf6c12d2fde664cd1e268488bc2c65
Jeongwoo Kim <corc411@gmail.com>
2024-04-15 16:14:53 +0900

    Refactor filtering logic and expand BadRequest handling in CommonService
    This commit introduces several significant changes to the CommonService and associated constants. It disables an ESLint rule at the top of the file to allow unused vars, presumably for future use. The BadRequestException has been added to improve error handling when incoming filter keys do not match expected patterns. Furthermore, the commit introduces a new constant file `filter-mapper.const.ts` which defines a mapping for TypeORM filter operations. This mapping is now utilized in the `parseWhereFilter` method to support a variety of filter operations based on the field and operator extracted from the key. This enhancement facilitates more dynamic query building based on client-provided filters.

--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { BadRequestException, Injectable } from '@nestjs/common';
 import {
   FindManyOptions,
   FindOptionsOrder,
   FindOptionsWhere,
   Repository,
 } from 'typeorm';
+import { FILTER_MAPPER } from './const/filter-mapper.const';
 import { BasePaginationDto } from './dto/base-pagination.dto';
 import { BaseModel } from './entity/base.entity';
 
@@ -61,7 +64,36 @@ export class CommonService {
   private parseWhereFilter<T extends BaseModel>(
     key: string,
     value: any,
-  ): FindOptionsWhere<T> {}
+  ): FindOptionsWhere<T> {
+    const options: FindOptionsWhere<T> = {};
+
+    const split = key.split['__'];
+
+    if (split.length !== 2 && split.length !== 3) {
+      throw new BadRequestException(
+        `where 필터는 '__'로 split했을 때 길이가 2 또는 3이어야 합니다 - 문제되는 키값: ${key}`,
+      );
+    }
+
+    if (split.length == 2) {
+      const [_, field] = split;
+      options[field] = value;
+    } else {
+      const [_, field, operator] = split;
+
+      // const values = value.toString().split(',');
+
+      // if (operator === 'between') {
+      //   options[field] = FILTER_MAPPER[operator](value[0], values[1]);
+      // } else {
+      //   options[field] = FILTER_MAPPER[operator](value);
+      // }
+
+      options[field] = FILTER_MAPPER[operator](value);
+    }
+
+    return options;
+  }
 
   private parseOrderFilter<T extends BaseModel>(
     key: string,
