6f0ab528d98f9633f4e2b9286bf29a6d53493799
Jeongwoo Kim <corc411@gmail.com>
2026-05-05 23:52:21 +0900

    feat(posts): configure image uploads

--- src/posts/entities/posts.entity.ts
+++ src/posts/entities/posts.entity.ts
@@ -17,6 +17,9 @@ export class PostsModel extends BaseModel {
   @IsString({ message: stringValidationMessage })
   content!: string;
 
+  @Column({ nullable: true })
+  image?: string;
+
   @Column()
   likeCount!: number;
 
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,8 +1,13 @@
-import { Module } from '@nestjs/common';
+import { BadRequestException, Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import multer from 'multer';
+import { extname } from 'path';
 import { AuthModule } from 'src/auth/auth.module';
 import { CommonModule } from 'src/common/common.module';
+import { POST_IMAGE_PATH } from 'src/common/const/path.const';
 import { UsersModule } from 'src/users/users.module';
+import { v4 } from 'uuid';
 import { PostsModel } from './entities/posts.entity';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
@@ -13,6 +18,27 @@ import { PostsService } from './posts.service';
     AuthModule,
     CommonModule,
     UsersModule,
+    MulterModule.register({
+      limits: { fileSize: 10000000 },
+      fileFilter: (req, file, cb) => {
+        const ext = extname(file.originalname);
+        if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
+          return cb(
+            new BadRequestException('jpg/jpeg/png 파일만 업로드 가능합니다.'),
+            false,
+          );
+        }
+        return cb(null, true);
+      },
+      storage: multer.diskStorage({
+        destination: function (req, res, cb) {
+          cb(null, POST_IMAGE_PATH);
+        },
+        filename: function (req, file, cb) {
+          cb(null, `${v4()}${extname(file.originalname)}`);
+        },
+      }),
+    }),
   ],
   controllers: [PostsController],
   providers: [PostsService],
